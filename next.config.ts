a8b1fd1bca0a133c9f18d696a3734aab69855aaa
Tashane Mclean <tashane.mclean@gmail.com>
2025-10-24 22:55:46 +0700

    Remove Prisma plugin from Next.js configuration and clean up package.json by eliminating unused dependencies.

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 const nextConfig: NextConfig = {
 	/* config options here */
-
-	// 💡 ADD THIS EMPTY CONFIG
-	// This explicitly tells Next.js to avoid Turbopack defaults in a way that respects the Webpack config.
-	turbopack: {},
-
-	webpack: (config, { isServer }) => {
-		if (isServer) {
-			config.plugins.push(new PrismaPlugin());
-		}
-
-		return config;
-	},
 };
 
 export default nextConfig;
