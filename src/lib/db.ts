c54b4f7a228591d170ee1584d5fe709b9ddd0f4b
Tashane Mclean <tashane.mclean@gmail.com>
2025-10-24 19:34:34 +0700

    Refactor package.json and package-lock.json to remove unused dependencies, update Prisma configuration, and enhance build scripts for improved database connectivity and performance.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,23 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
 
-import { neonConfig } from "@neondatabase/serverless";
-import ws from "ws";
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-neonConfig.webSocketConstructor = ws;
-
-const connectionString = process.env.DATABASE_URL;
-
-const adapter = new PrismaNeon({ connectionString });
-
-const globalForPrisma = global as unknown as {
-	prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-	globalForPrisma.prisma ||
-	new PrismaClient({
-		adapter: adapter,
-	});
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
